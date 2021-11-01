70e68937a267fdbc59f55e055bcfa6444f2007e3
Krista Dotzenrod <kdotzenrod517@gmail.com>
2021-11-01 18:06:53 -0500

    Add email

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
 module.exports = {
   reactStrictMode: true,
-}
+  env: {
+    NEXT_PUBLIC_SERVICE_ID: process.env.NEXT_PUBLIC_SERVICE_ID,
+    NEXT_PUBLIC_TEMPLATE_ID: process.env.NEXT_PUBLIC_TEMPLATE_ID,
+    NEXT_PUBLIC_USER_ID: process.env.NEXT_PUBLIC_USER_ID,
+  },
+};
